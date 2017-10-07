a3d6b32f7d32c714c32cd334fcf4db1b2f37ec1f
Praveen Kumar Bonakurthi <prbona@microsoft.com>
2017-10-08 01:07:36 +0530

    initial commit

---
+++ app/src/main/java/com/example/bpraveen4u/gbooks/BookListActivity.java
@@ -0,0 +1,143 @@
+package com.example.bpraveen4u.gbooks;
+
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v4.view.MenuItemCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.SearchView;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ArrayList;
+
+public class BookListActivity extends AppCompatActivity implements SearchView.OnQueryTextListener{
+    private ProgressBar mLoadingProgress;
+    private RecyclerView rvBooks;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_book_list);
+        mLoadingProgress = (ProgressBar) findViewById(R.id.pb_loading);
+        rvBooks = (RecyclerView) findViewById(R.id.rv_books);
+        LinearLayoutManager booksLayoutManager = new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false);
+        rvBooks.setLayoutManager(booksLayoutManager);
+        try {
+            URL bookUrl = ApiUtil.buildUrl("android");
+            new BooksQueryTask().execute(bookUrl);
+        }
+        catch (Exception e){
+            Log.d("Error", e.getMessage());
+        }
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+       FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_book_list, menu);
+        final MenuItem searchItem = menu.findItem(R.id.action_search);
+        final SearchView searchView = (SearchView) MenuItemCompat.getActionView(searchItem);
+        searchView.setOnQueryTextListener(this);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        //if (id == R.id.action_settings) {
+        //    return true;
+        //}
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public boolean onQueryTextSubmit(String query) {
+        try{
+            URL bookUrl = ApiUtil.buildUrl(query);
+            new BooksQueryTask().execute(bookUrl);
+        }
+        catch (Exception e){
+            Log.d("Error", e.getMessage());
+        }
+        return false;
+    }
+
+    @Override
+    public boolean onQueryTextChange(String newText) {
+        return false;
+    }
+
+    public class BooksQueryTask extends AsyncTask<URL, Void, String> {
+
+        @Override
+        protected String doInBackground(URL... urls) {
+            URL searchUrl = urls[0];
+            String result = null;
+            try {
+                result = ApiUtil.getJson(searchUrl);
+            }
+            catch(IOException e) {
+                Log.d("Error", e.getMessage());
+            }
+            return result;
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            //TextView tvResult = (TextView) findViewById(R.id.tvResponse);
+            TextView tvError = (TextView) findViewById(R.id.tv_error);
+            mLoadingProgress.setVisibility(View.INVISIBLE);
+            if (result == null) {
+                rvBooks.setVisibility(View.INVISIBLE);
+                tvError.setVisibility(View.VISIBLE);
+            }
+            else{
+                rvBooks.setVisibility(View.VISIBLE);
+                tvError.setVisibility(View.INVISIBLE);
+            }
+            //tvResult.setText(result);
+            ArrayList<Book> books = ApiUtil.getBooksFromJson(result);
+            String resultString = "";
+            /*for(Book book: books){
+                resultString = resultString + book.title + "\n" + book.publishedDate + "\n\n";
+            }
+            tvResult.setText(resultString);
+            */
+            BooksAdapter adapter = new BooksAdapter(books);
+            rvBooks.setAdapter(adapter);
+        }
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            mLoadingProgress.setVisibility(View.VISIBLE);
+        }
+    }
+}
